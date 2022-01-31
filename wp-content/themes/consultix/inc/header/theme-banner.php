816ad7f04219696b3a4221f2b8fbf0b34739e863
kuba <info@weblead.pl>
2022-01-31 16:17:55 +0100

    pierwszy

--- wp-content/themes/consultix/inc/header/theme-banner.php
+++ wp-content/themes/consultix/inc/header/theme-banner.php
@@ -867,7 +867,9 @@ if ( is_front_page() && is_home() ) { ?>
 							<!-- inner_banner_main -->
 							<div class="inner_banner_main">
 								<?php if ( get_post_meta( $post->ID, 'banner_title', true ) ) : ?>
-									<?php var_dump($post->ID) ; ?>
+									<?php var_dump($post->ID) ;
+									echo 'kuba123';
+									?>
 									<p class="title">
 										<?php echo esc_html( get_post_meta( $post->ID, 'banner_title', true ) ); ?>
 									</p>
